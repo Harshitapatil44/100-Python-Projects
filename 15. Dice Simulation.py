e6f1190af76e5b479dfac8f19e02ec054eb676e5
harshita Patil <patilharshita347@gmail.com>
2026-05-10 12:34:02 +0530

    Update 15. Dice Simulation.py

--- 15. Dice Simulation.py
+++ 15. Dice Simulation.py
@@ -1,1 +1,17 @@
+import random 
+
+def roll_dice():
+  return random.randit(1, 6)
+
+while True: 
+  input("Press Enter to roll dice....!")
+  
+  result = roll_dice()
+  print(f"You Rolled: " {result}")
+  
+  choice = input("if Want to roll again? press Y/N for your ans").lower()
+  
+  if choice  != "y":
+      print("Thank you for Playing")
+      break
 
