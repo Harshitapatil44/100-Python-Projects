3359aee21c1be197562c72a054d08adb014543e9
harshita Patil <patilharshita347@gmail.com>
2026-06-03 13:55:40 +0530

    Update 23. Task Sheduller.py

--- 23. Task Sheduller.py
+++ 23. Task Sheduller.py
@@ -1,1 +1,20 @@
+import sched
+import time
 
+# Initialize the scheduler
+s = sched.scheduler(time.time, time.sleep)
+
+def print_time(a='default'):
+    print("From print_time", time.time(), a)
+
+def schedule_tasks():
+    print("Start time:", time.time())
+    
+    # arguments: (delay_in_seconds, priority, function, argument_tuple)
+    s.enter(5, 1, print_time, argument=('first job',))
+    s.enter(10, 1, print_time, argument=('second job',))
+
+    # Run the scheduled events
+    s.run()
+
+schedule_tasks()
