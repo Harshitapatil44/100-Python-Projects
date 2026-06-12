0eb14cc5e687b7d524e8affacc077e2bdd445bb0
harshita Patil <patilharshita347@gmail.com>
2026-06-12 23:13:13 +0530

    Update 28.Text Editor.py

--- 28.Text Editor.py
+++ 28.Text Editor.py
@@ -1,1 +1,42 @@
+class TextEditor:
+    def __init__(self):
+        self.text = ""
 
+    def write(self, content):
+        self.text += content
+
+    def show(self):
+        print("\nCurrent Text:")
+        print(self.text)
+
+    def clear(self):
+        self.text = ""
+        print("Text Cleared")
+
+
+editor = TextEditor()
+
+while True:
+    print("\n1. Write Text")
+    print("2. Show Text")
+    print("3. Clear Text")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        content = input("Enter text: ")
+        editor.write(content + "\n")
+
+    elif choice == "2":
+        editor.show()
+
+    elif choice == "3":
+        editor.clear()
+
+    elif choice == "4":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid Choice")
