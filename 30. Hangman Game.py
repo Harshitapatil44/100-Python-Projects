300be38984d4d0f6bc786299348d4742fd3f2c46
harshita Patil <patilharshita347@gmail.com>
2026-06-16 10:27:12 +0530

    Update 30. Hangman Game.py

--- 30. Hangman Game.py
+++ 30. Hangman Game.py
@@ -1,1 +1,37 @@
+word = "python"
+guessed = []
+attempts = 6
 
+print("Welcome to Hangman!")
+
+while attempts > 0:
+    display = ""
+
+    for letter in word:
+        if letter in guessed:
+            display += letter + " "
+        else:
+            display += "_ "
+
+    print("\nWord:", display)
+
+    if "_" not in display:
+        print("You won!")
+        break
+
+    guess = input("Enter a letter: ").lower()
+
+    if guess in guessed:
+        print("Already guessed!")
+        continue
+
+    guessed.append(guess)
+
+    if guess not in word:
+        attempts -= 1
+        print("Wrong guess!")
+        print("Attempts left:", attempts)
+
+else:
+    print("\nYou lost!")
+    print("The word was:", word)
