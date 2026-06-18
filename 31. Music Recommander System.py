48e2fbbe10ccb994566ef2cf4e741ca49496a2f0
harshita Patil <patilharshita347@gmail.com>
2026-06-18 14:04:26 +0530

    Update 31. Music Recommander System.py

--- 31. Music Recommander System.py
+++ 31. Music Recommander System.py
@@ -1,1 +1,19 @@
+music = {
+    "Pop": ["Shape of You", "Blinding Lights", "Levitating"],
+    "Rock": ["Believer", "Thunderstruck", "Numb"],
+    "Hip-Hop": ["God's Plan", "Lose Yourself", "Sicko Mode"],
+    "Classical": ["Moonlight Sonata", "The Four Seasons", "Canon in D"]
+}
 
+print("Music Genres:")
+for genre in music:
+    print("-", genre)
+
+choice = input("\nEnter your favorite genre: ")
+
+if choice in music:
+    print("\nRecommended Songs:")
+    for song in music[choice]:
+        print(song)
+else:
+    print("Genre not found.")
