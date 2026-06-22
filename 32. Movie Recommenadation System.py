2fa5b984748a37c3380e108c6bd31af6c2fa680b
harshita Patil <patilharshita347@gmail.com>
2026-06-22 15:08:41 +0530

    Update 32. Movie Recommenadation System.py

--- 32. Movie Recommenadation System.py
+++ 32. Movie Recommenadation System.py
@@ -1,1 +1,19 @@
+movies = {
+    "Action": ["Avengers", "Batman", "John Wick"],
+    "Comedy": ["3 Idiots", "Dhamaal", "Golmaal"],
+    "Horror": ["The Conjuring", "Annabelle", "Insidious"],
+    "Sci-Fi": ["Interstellar", "Inception", "The Matrix"]
+}
 
+print("Available Genres:")
+for genre in movies:
+    print("-", genre)
+
+choice = input("\nEnter your favorite genre: ")
+
+if choice in movies:
+    print("\nRecommended Movies:")
+    for movie in movies[choice]:
+        print(movie)
+else:
+    print("Sorry, genre not found.")
