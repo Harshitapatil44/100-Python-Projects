437a1a6d5d9754a049d9f20a5bfb7d706d80b232
harshita Patil <patilharshita347@gmail.com>
2026-06-23 15:44:47 +0530

    Update 33. Chat Application.py

--- 33. Chat Application.py
+++ 33. Chat Application.py
@@ -12,7 +12,6 @@ while True:
         msg = input("Enter message: ")
         messages.append(msg)
         print("Message sent!")
-
     elif choice == "2":
         print("\nChat History:")
         if len(messages) == 0:
@@ -20,10 +19,8 @@ while True:
         else:
             for i, msg in enumerate(messages, start=1):
                 print(f"{i}. {msg}")
-
     elif choice == "3":
         print("Exiting chat...")
         break
-
     else:
         print("Invalid choice! Try again.")
