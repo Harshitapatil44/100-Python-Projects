3b1213d538b0cef223a836680c3cf29f15e92666
harshita Patil <patilharshita347@gmail.com>
2026-06-26 15:43:03 +0530

    Update 34 Password Strength Checker.py

--- 34 Password Strength Checker.py
+++ 34 Password Strength Checker.py
@@ -1,1 +1,33 @@
+password = input("Enter your password: ")
 
+strength = 0
+
+# Length check
+if len(password) >= 8:
+    strength += 1
+
+# Uppercase check
+if any(char.isupper() for char in password):
+    strength += 1
+
+# Lowercase check
+if any(char.islower() for char in password):
+    strength += 1
+
+# Digit check
+if any(char.isdigit() for char in password):
+    strength += 1
+
+# Special character check
+special = "!@#$%^&*()_+-=[]{}|;:',.<>?/"
+
+if any(char in special for char in password):
+    strength += 1
+
+# Result
+if strength == 5:
+    print("Strong Password")
+elif strength >= 3:
+    print("Medium Password")
+else:
+    print("Weak Password")
